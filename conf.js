dbee63ea4812ca631655456b9a23ffdc6156c7ac
LakshmipriyaBandi <31640815+LakshmipriyaBandi@users.noreply.github.com>
2017-09-05 10:02:12 +0530

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,30 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  /*capabilities: {
+    'browserName': 'chrome'
+  }, */
+  
+  multiCapabilities: [{
+  'browserName': 'chrome',
+  'count': 4
+} , {
+  'browserName': 'firefox',
+  'count': 2
+},
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['example_spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000,
+	allScriptsTimeout: 30000
+  }
+};
